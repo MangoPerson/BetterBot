fc3bafeec44b1426c852bf7bd06b2babf75c465e
Cody <codyharding06@gmail.com>
2024-03-26 22:48:00 -0400

    First Commit of BetterBot v2

---
+++ functions.py
@@ -0,0 +1,47 @@
+import PyDictionary
+import random
+def setup():
+    # AI STUFF
+    import google.generativeai as genai
+    genai.configure()
+    model = genai.GenerativeModel('gemini-pro')
+    # DISCORD SETUP
+    import discord
+    intents = discord.Intents.all()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    return client, model
+
+
+def ReturnDefinition(word):
+    try:
+        return f'The definition of {word} is \n{PyDictionary.PyDictionary.meaning(word)}'
+    except:
+        return 'Something went wrong!'
+
+
+def GenerateAiResponse(prompt, model):
+    try:
+        return model.generate_content(prompt).text
+    except:
+        return 'Something went wrong!'
+
+
+def Mock(user, users, guild):
+
+    # print(globals()[str(user).lower() + '_' + str(guild).lower()].name)
+    # print(users)
+    # print(guild)
+
+    return
+
+    if user not in [r[0] for r in users]:
+        return 'Invalid user, possible user are'
+
+    if user.IsBeingMocked:
+        return 'User is already being mocked'
+    else:
+        user.IsBeingMocked = True
+        user.UpdateMock()
+        return 'User is now being mocked'
--- main.py
+++ main.py
@@ -1,369 +1,81 @@
 
+import functions
 
-import random
-
-import PyDictionary
-
-import discord
-
-import re
-
-
-
-# AI STUFF
-
-import google.generativeai as genai
-
-genai.configure(api_key='NOT AVAILABLE')
-
-model = genai.GenerativeModel('gemini-pro')
-
-
-
-# DISCORD SETUP
-
-intents = discord.Intents.all()
-
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-
-
-
+client, model = functions.setup()
 
 class DiscordPerson:
-
-    def __init__(self, name):
-
+    def __init__(self, name, guild):
         self.TimesMocked = 0
-
         self.name = name
-
         self.IsBeingMocked = False
-
         self.ShouldMock = False
-
-
-
+        self.guild = guild
     def UpdateMock(self):
-
         if self.IsBeingMocked == True:
-
             self.ShouldMock = True
-
-            if self.TimesMocked + 1 >= 10:
-
+            if self.TimesMocked + 1 % 3 == 0:
                 self.ShouldMock = False
-
                 self.IsBeingMocked = False
-
-                self.TimesMocked = 0
-
             else:
-
                 self.TimesMocked += 1
 
-
-
-
-
     def ReturnStats(self):
+        return vars(self)
 
-        return {
-
-            'Times Mocked': self.TimesMocked,
-
-            'Name': self.name,
-
-            'Is being mocked': self.IsBeingMocked
-
-        }
-
-
-
-# FIRST RUN THROUGH
+def CheckIfCommandIsValid(message):
+    Regmsg = message.content.lower().split(' ')
+    if len(Regmsg) < 3:
+        return False
+    return True
 
 @client.event
-
 async def on_ready():
+    global MemberNameTags
+    print(f'logged in as {client.user}\n')
+    MemberNameTags = []
+    for server in client.guilds:
+        for member in server.members:
+            MemberNameTags.append((str(member.name).lower() + '@' + str(member.guild.name).lower()))
 
-    print(f'We have logged in as {client.user}')
+    for i in range(len(MemberNameTags)):
+        globals()[f'{MemberNameTags[i]}'] = DiscordPerson(*MemberNameTags[i].split('@'))
 
-    global MEMBERS
-
-    MEMBERS = []
-
-    for member in client.get_all_members():
-
-        MEMBERS.append(member)
-
-    global MembersList
-
-    MembersList = []
-
-    MembersList = [str(MEMBERS[x]) for x in range(len(MEMBERS))]
-
-
-
-    for i in range(len(MembersList)):
-
-        globals()[f'{MembersList[i]}'] = DiscordPerson(MembersList[i])
 
 
 
 @client.event
-
 async def on_message(message):
-
-
-
-    print(f'Message from {message.author}:\n{message.content}')
-
+    global MemberNameTags
+    Regmsg = message.content.lower().split(' ')
+    send = message.channel.send
     if message.author == client.user:
-
         return
+    print(f'{message.author.name}\n{message.content}')
+    if Regmsg[0] in ['$bb', '$betterbot']:
 
-    # Test to see if user should be mocked
-
-    try:
-
-        if globals()[f'{message.author}'].ShouldMock:
-
-            globals()[f'{message.author}'].UpdateMock()
-
-            sentence = '*'
-
-            for i in range(len(message.content.lower())):
-
-                sentence += str(random.choice([message.content.lower()[i].upper(), message.content.lower()[i].lower()]))
-
-            sentence += '*'
-
-
-
-
-
-            await message.channel.send(sentence)
-
-    except:
-
-        await message.channel.send("Something went wrong!")
-
-        raise Exception
-
-        return
-
-
-
-    if message.content.lower().startswith('$betterbot') or message.content.lower().startswith('$bb'):
-
-
-
-        Regmsg = message.content.lower().split(' ')
-
-        send = message.channel.send
-
-
-
-        # ERROR CORRECTION
-
-        if len(Regmsg) == 1:
-
+        if not CheckIfCommandIsValid(message):
             await send('Invalid command, please enter a proper command')
-
             return
+        if Regmsg[1] in ['dict', 'dictionary', 'define']:
+            await send(functions.ReturnDefinition(Regmsg[2]))
 
-        if len(Regmsg) < 3 :
+        if Regmsg[1] == 'ai':
+            await send(functions.GenerateAiResponse([' '.join(str(x) for x in Regmsg[2:])][0], model))
 
-            if Regmsg[1] == 'help':
-
-                try:
-
-                    await send('help: this command\n'
-
-                               'say [prompt]: will say whatever is in the prompt\n'
-
-                               '[dict, define, dictionary] [word]: using any of the first commands, it will define [word]\n'
-
-                               'ai [prompt] will run prompt through an LLM\n'
-
-                               'mock [user] will start mocking [user] for the next 10 prompts\n'
-
-                               'stats [user] will return statistics about [user]')
-
-                except:
-
-                    await send('Something went wrong!')
-
-                    raise Exception
-
-                    return
-
-                return
-
-            await send('Invalid command, please enter a proper command')
-
+        if Regmsg[1] == 'say':
+            content = [' '.join(str(x) for x in Regmsg[2:])][0]
+            await send(content)
             return
-
-
-
-
-
-        # DICTIONARY CODE
-
-        try:
-
-            if Regmsg[1] in ['dict', 'dictionary', 'define']:
-
-                if Regmsg[2] == '*help':
-
-                    await send(f'The definition of help is :\n{PyDictionary.PyDictionary.meaning('help')}')
-
-                    return
-
-                if Regmsg[2] == 'help':
-
-                    await send('To use the dictionary feature, write $bb [\'dict\', \'dictionary\', or \'define\'] WORD_TO_DEFINE \nTo define help, re-run the command with \'*help\'')
-
-                    return
-
-                await send(f'The definition of {Regmsg[2]} is :\n{PyDictionary.PyDictionary.meaning(Regmsg[2])}')
-
-                return
-
-        except:
-
-            await send('Something went wrong!')
-
-            raise Exception
-
-            return
-
-
-
-        # AI CODE
-
-        try:
-
-            if Regmsg[1] in ['ai']:
-
-                await send('*Getting Response*')
-
-                content = [' '.join(str(x) for x in Regmsg[2:])][0]
-
-                print(model.generate_content(content).text)
-
-                await send(model.generate_content(content).text)
-
-                return
-
-        except:
-
-            await send('Something went wrong!')
-
-            raise Exception
-
-            return
-
-
-
-        # SAY CODE
-
-        try:
-
-            if Regmsg[1] == 'say':
-
-                content = [' '.join(str(x) for x in Regmsg[2:])][0]
-
-                await send(content)
-
-                return
-
-        except:
-
-            await send('Something went wrong!')
-
-            raise Exception
-
-            return
-
-
-
-        # MOCKING CODE
-
-        try:
-
-            global MembersList
-
-            if Regmsg[1] == 'mock':
-
-                if Regmsg[2] not in MembersList:
-
-                    await send(f'User \'{Regmsg[2]}\' is not a valid member.\nValid members are: \n{MembersList}')
-
-                    return
-
-                else:
-
-                    if globals()[Regmsg[2]].IsBeingMocked == False:
-
-                        globals()[Regmsg[2]].IsBeingMocked = True
-
-                        globals()[Regmsg[2]].UpdateMock()
-
-                        await send(f'User {Regmsg[2]} is now being mocked')
-
-                    else:
-
-                        await send(f'User {Regmsg[2]} is already being mocked')
-
-                        return
-
-        except:
-
-            await send('Something went Wrong!')
-
-            raise Exception
-
-            return
-
-
-
-        # STATS CODE
-
-        try:
-
-            if Regmsg[1] == 'stats':
-
-                if Regmsg[2] not in MembersList:
-
-                    await send(f'User \'{Regmsg[2]}\' is not a valid member.\nValid members are: \n{MembersList}')
-
-                    return
-
-                else:
-
-                    await send(globals()[Regmsg[2]].ReturnStats())
-
-                    return
-
-        except:
-
-            await send('Something wend wrong!')
-
-            raise Exception
-
-            return
-
-
-
-
-
-
-
-
-
-
-
-client.run('NOT AVAILABLE')
+        if Regmsg[1] == 'mock':
+            try:
+                globals()[MemberNameTags]
+            except:
+                print(message.author.guild)
+                ValidUsers = []
+                for i in MemberNameTags:
+                    if i.split('@')[1] == str(message.author.guild).lower():
+                        ValidUsers.append(i.split('@')[0])
+
+                await send(f'Invalid user to mock, valid people are:\n{ValidUsers}')
+
+client.run()
