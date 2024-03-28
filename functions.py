f2e02c32028208da7c53b693295aa95fa102293c
Cody <codyharding06@gmail.com>
2024-03-27 22:20:23 -0400

    Implemented Mock
    Created a system that will distinguish between the same user but on different servers (Also in Mock)
    
    Commented Out most of the code to make it easy to read

--- functions.py
+++ functions.py
@@ -5,6 +5,8 @@ import google.generativeai as genai
 import discord
 
 
+# This will run on first setup
+# Initializes the AI and Discord Imports
 def setup():
     # AI STUFF
     genai.configure(api_key=os.environ['AI_KEY'])
@@ -17,42 +19,45 @@ def setup():
 
     return client, model
 
-
+# Will ensure the command has the proper format and number of arguments
 def CheckIfCommandIsValid(message):
     Regmsg = message.content.lower().split(' ')
     if len(Regmsg) < 2:
         return False
     return True
 
-
+# This will return a definition of a word using PyDictionary
 def ReturnDefinition(word):
     try:
         return f'The definition of {word} is \n{PyDictionary.PyDictionary.meaning(word)}'
     except:
         return 'Something went wrong!'
 
-
+# This will use Google's API to return a respons from a given prompt
 def GenerateAiResponse(prompt, model):
     try:
         return model.generate_content(prompt).text
     except:
         return 'Something went wrong!'
 
-
-def Mock(user, users, guild):
-
-    # print(globals()[str(user).lower() + '_' + str(guild).lower()].name)
-    # print(users)
-    # print(guild)
-
-    return
-
-    if user not in [r[0] for r in users]:
-        return 'Invalid user, possible user are'
-
-    if user.IsBeingMocked:
+# This is overly complicated and I don't feel like explaining it
+# It works, DONT TOUTCH IT
+# pls :3
+def Mock(user):
+    if user.ShouldMock:
         return 'User is already being mocked'
     else:
-        user.IsBeingMocked = True
-        user.UpdateMock()
+        user.ShouldMock = True
         return 'User is now being mocked'
+
+# It does what it says
+def Stats(user):
+    return user.ReturnStats()
+
+# Again, does what it says
+def ScrambleLetters(string):
+    print(string)
+    ReturnVal = ''
+    for i in string:
+        ReturnVal += random.choice((i.lower(), i.upper()))
+    return ReturnVal
--- main.py
+++ main.py
@@ -11,69 +11,111 @@ class DiscordPerson:
     def __init__(self, name, guild):
         self.TimesMocked = 0
         self.name = name
-        self.IsBeingMocked = False
         self.ShouldMock = False
         self.guild = guild
     def UpdateMock(self):
-        if self.IsBeingMocked == True:
-            self.ShouldMock = True
-            if self.TimesMocked + 1 % 3 == 0:
-                self.ShouldMock = False
-                self.IsBeingMocked = False
-            else:
-                self.TimesMocked += 1
+        self.TimesMocked += 1
+        if (self.TimesMocked + 1) % 4 == 0:
+            self.ShouldMock = False
+
 
     def ReturnStats(self):
         return vars(self)
 
-
+# This code runs once the bot logs into the server(s)
 @client.event
 async def on_ready():
+    # Make the user name tags global
     global MemberNameTags
+
+    # Condfirmation on login
     print(f'logged in as {client.user}\n')
+
+    # Generate usernames in the format "NAME@SERVER" all lowercase
     MemberNameTags = []
     for server in client.guilds:
         for member in server.members:
             MemberNameTags.append((str(member.name).lower() + '@' + str(member.guild.name).lower()))
 
+    # Create global variables of all users with the DiscordPerson class
     for i in range(len(MemberNameTags)):
         globals()[f'{MemberNameTags[i]}'] = DiscordPerson(*MemberNameTags[i].split('@'))
 
 
+# This will run every time a message is sent
 @client.event
 async def on_message(message):
+
+    # This variable has the "name tags" for all of the user's DiscordPerson class
     global MemberNameTags
+
+    # Converts the input into usable chunks
     Regmsg = message.content.lower().split(' ')
+
+    # Shorthand for message.channel.send()
     send = message.channel.send
+
+    # This will make the bot not talk to itself
     if message.author == client.user:
         return
     print(f'{message.author.name}\n{message.content}')
+
+    # This will check if a user should be mocked
+    if globals()[str(message.author.name).lower() + '@' + str(message.author.guild).lower()].ShouldMock:
+        globals()[str(message.author.name).lower() + '@' + str(message.author.guild).lower()].UpdateMock()
+        await send(functions.ScrambleLetters(message.content))
+
+    # This will only run when the message starts with $bb or $betterbot
     if Regmsg[0] in ['$bb', '$betterbot']:
 
+        # Makes sure that the command has the prefix "$bb", a command "stats, mock, etc" and an argument, like user or word
         if not functions.CheckIfCommandIsValid(message):
             await send('Invalid command, please enter a proper command')
             return
+
+        # Return definition of the given word
         if Regmsg[1] in ['dict', 'dictionary', 'define']:
             await send(functions.ReturnDefinition(Regmsg[2]))
 
+        # Will generate an AI response with the user's prompt
         if Regmsg[1] == 'ai':
             await send(functions.GenerateAiResponse([' '.join(str(x) for x in Regmsg[2:])][0], model))
 
+        # Will just copy what the user's message is
         if Regmsg[1] == 'say':
             content = [' '.join(str(x) for x in Regmsg[2:])][0]
             await send(content)
             return
+
+        # Will initialize the mocking procedure
+        # This will set the user's .ShouldMock attribute to true, and while
+        # It is true, the bot will "mock" the user
         if Regmsg[1] == 'mock':
             try:
-                globals()[MemberNameTags]
+                # Get the user's class variable
+                await send(functions.Mock(globals()[str(Regmsg[2]).lower() + '@' + str(message.author.guild).lower()]))
             except:
-                print(message.author.guild)
+                # Return a list of possible users for a given server
+                ValidUsers = []
+                for i in MemberNameTags:
+                    if i.split('@')[1] == str(message.author.guild).lower():
+                        ValidUsers.append(i.split('@')[0])
+                await send(f'Invalid user to mock, valid people are:\n{ValidUsers}')
+
+        # Return Stats of a given user
+        if Regmsg[1] == 'stats':
+            try:
+                await send(functions.Stats(globals()[str(Regmsg[2]).lower() + '@' + str(message.author.guild).lower()]))
+            except:
                 ValidUsers = []
                 for i in MemberNameTags:
                     if i.split('@')[1] == str(message.author.guild).lower():
                         ValidUsers.append(i.split('@')[0])
 
-                await send(f'Invalid user to mock, valid people are:\n{ValidUsers}')
+                await send(f'Invalid user for stats, valid people are:\n{ValidUsers}')
+
+        # MangoPerson PLEASE COMMENT THIS FUNCTION * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+        # Cant comment this because idk what it does
         if Regmsg[1] == 'amogus':
             await handle(client, message)
 
